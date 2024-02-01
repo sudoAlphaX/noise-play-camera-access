f9884c847b4a2a4658ced714779ec61fbaf4989a
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-02-01 20:38:25 +0530

    add resolution set

--- camera/settings.py
+++ camera/settings.py
@@ -9,8 +9,20 @@ def setTime(time=datetime.datetime.now(), ip="192.72.1.1"):
         )
     )
 
-    if data.status_code == 200:
+def setResolution(res, ip="192.72.1.1"):
+    data = requests.get(
+        "http://{}/cgi-bin/Config.cgi?action=set&property=Videores&value={}".format(
+            ip, res
+        )
+    )
+
+    if data.text == "0\nOK\n":
         return True
 
     else:
         return False
+
+
+if __name__ == "__main__":
+    data = setResolution("2.7K15")
+    print(data)
